88d06f491ee93dc59e82f69f293ebab7d118457d
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2024-06-15 21:04:50 -0700

    Update HomeCTA.tsx

--- apps/landing/src/components/HomeCTA.tsx
+++ apps/landing/src/components/HomeCTA.tsx
@@ -14,7 +14,7 @@ export function HomeCTA({ className, text, icon, ...props }: Props) {
 		<Button
 			size="lg"
 			className={clsx(
-				'home-button-border-gradient relative z-30 flex cursor-pointer items-center gap-2 !rounded-[7px] border-0 !bg-[#2F3152]/30 py-2 text-sm !backdrop-blur-lg hover:brightness-110 md:text-[16px]',
+				'home-button-border-gradient relative z-30 flex cursor-pointer items-center gap-2 !rounded-[7px] border-0 !bg-white py-2 text-sm !backdrop-blur-lg hover:brightness-110 md:text-[16px]',
 				className
 			)}
 			{...props}
